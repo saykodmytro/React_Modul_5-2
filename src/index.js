240d563b2c28fd606205cc2793b4db1a9f4ec91d
Dmytro Sayko <saykodmytro@icloud.com>
2023-11-20 17:45:37 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ModalContextProvider>
-    <BrowserRouter>
+    <BrowserRouter basename="/React_Modul_5-2">
       <App />
     </BrowserRouter>
   </ModalContextProvider>
